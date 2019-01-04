dabd68c67906ae6ce3330fadbaa956d2b540bff2
Christie Lynam <christielynam@gmail.com>
2019-01-04 12:34:33 -0700

    Update starter repo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import PropTypes, { shape, func, string } from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
-import { connect } from 'react-redux';
-import { fakeAction } from '../../actions';
+
 class App extends Component {
 
   render() {
@@ -12,10 +10,6 @@ class App extends Component {
         <div className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to Westeros</h2>
-          <button onClick={() => {
-            this.props.fakeAction();
-            alert(this.props.fake);
-          }}> FAKE ACTION</button>
         </div>
         <div className='Display-info'>
         </div>
@@ -24,13 +18,4 @@ class App extends Component {
   }
 }
 
-App.propTypes = {
-  fake: shape({ fake: string }),
-  fakeAction: func.isRequired
-};
-
-const mapStateToProps = ({ fake }) => ({ fake });
-const mapDispatchToProps = dispatch => ({ fakeAction:
-  () => dispatch(fakeAction())
-});
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/reducers/root-reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { fake } from './fake-reducer';
-
-const rootReducer = combineReducers({
-  fake
-});
-
-
-export default rootReducer;
