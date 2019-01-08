9ce81e3e29dc98965a672284da92dff44fe73207
Ashley Levi <Ashplevi@gmail.com>
2019-01-08 09:16:09 -0700

    First commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className='App'>
         <div className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to Westeros</h2>
+          <h2>Welcome to Westeros!!</h2>
         </div>
         <div className='Display-info'>
         </div>
